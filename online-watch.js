f561b98c67b304255a21a05394923f4d30a1b101
TheFogIOF <70043840+TheFogIOF@users.noreply.github.com>
2025-04-12 11:22:27 +1000

    Add files via upload

--- online-watch.js
+++ online-watch.js
@@ -12,11 +12,11 @@
             }
         });
     
-        function appReady() {
+        function appReady(e) {
     
         }
     
-        function fullComplite() {
+        function fullComplite(e) {
             var watch_button = '<div class="full-start__button selector button--watch"> <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="14" cy="14.5" r="13" stroke="currentColor" stroke-width="2.7"></circle> <path d="M18.0739 13.634C18.7406 14.0189 18.7406 14.9811 18.0739 15.366L11.751 19.0166C11.0843 19.4015 10.251 18.9204 10.251 18.1506L10.251 10.8494C10.251 10.0796 11.0843 9.5985 11.751 9.9834L18.0739 13.634Z" fill="currentColor"></path> </svg> <span>Онлайн-просмотр</span> </div>';
             var btn = $(Lampa.Lang.translate(watch_button));
         }
@@ -24,12 +24,12 @@
         //events
     
         Lampa.Listener.follow('full', function(e) {
-            if (e.type == 'complite') fullComplite();
+            if (e.type == 'complite') fullComplite(e);
         });
     
         if (window.appready) appReady(); else {
             Lampa.Listener.follow('app', function(e) {
-                if (e.type == 'ready') appReady();
+                if (e.type == 'ready') appReady(e);
             });
         }
     }
